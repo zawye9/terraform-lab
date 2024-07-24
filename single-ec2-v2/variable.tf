236880198cc34f1cd552fa8cca1be40138db16ed
zawye9 <zawyegreenhat@gmail.com>
2024-07-25 00:36:37 +0630

    update single ec2 with var

---
+++ single-ec2-v2/variable.tf
@@ -0,0 +1,12 @@
+variable "http-port" {
+  type        = number
+  default     = 80
+  description = "http port"
+}
+
+variable "main" {
+  type        = string
+  default     = "vpc-8aa042ec"
+  description = "aws main vpc"
+}
+
