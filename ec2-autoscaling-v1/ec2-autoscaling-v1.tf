29566293c06166cde6f284794027543d564def95
zawye9 <zawyegreenhat@gmail.com>
2024-07-25 02:09:54 +0630

    update

--- ec2-autoscaling-v1/ec2-autoscaling-v1.tf
+++ ec2-autoscaling-v1/ec2-autoscaling-v1.tf
@@ -14,8 +14,9 @@ provider "aws" {
 resource "aws_launch_template" "web-scaling" {
     name = "web-scaling"
     image_id = var.image_id
+    instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.all-80.id]
-    user_data = ${data.template_file.userdata.rendered}
+    user_data = "${base64encode(data.template_file.userdata.rendered)}"
 }
 resource "aws_security_group" "all-80" {
     name = "all-80"
@@ -30,7 +31,7 @@ resource "aws_vpc_security_group_ingress_rule" "ingress-80" {
 }
 
 resource "aws_autoscaling_group" "web-auto" {
-    availability_zones = ["ap-southeat-1"]
+    availability_zones = ["ap-southeast-1a"]
     desired_capacity = 1
     min_size = 1
     max_size = 1
